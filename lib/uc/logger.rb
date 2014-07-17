6963aa801e2d77b3cfea9e8eb3574c8d447085e1
Neeraj <neeraj.bhunwal@gmail.com>
2014-07-17 12:15:25 +0530

    modify logger formatting, add support for stderr logs

--- lib/uc/logger.rb
+++ lib/uc/logger.rb
@@ -4,23 +4,36 @@ module Uc
   module Logger
 
     class << self
-      attr_accessor :event_queue
+      attr_reader :event_queue
+    end
+
+    def self.event_queue=(queue_name)
+      @event_stream = nil
+      @event_queue = queue_name
     end
 
     def self.logger
       @logger ||= begin
         logger = ::Logger.new(STDOUT)
         logger.formatter = proc do |severity, datetime, progname, msg|
-          if severity == "INFO"
+          case severity
+          when "INFO"
             "#{msg}\n"
+          when "ERROR"
+            "#{severity.downcase.bold.red} #{msg}\n"
           else
-            "#{severity} #{msg}\n"
+            "#{severity.downcase.bold.blue} #{msg}\n"
           end
         end
         logger.level = ::Logger::INFO
         logger
       end
     end
+
+    def self.stderr
+      @stderr ||= ::Logger.new(STDERR)
+    end
+
 
     def self.event_stream
       @event_stream ||= ::Uc::EventStream.new(event_queue)
@@ -34,5 +47,12 @@ module Uc
       ::Uc::Logger.logger
     end
 
+    def stderr
+      ::Uc::Logger.stderr
+    end
+
+    def event_queue
+      ::Uc::Logger.event_queue
+    end
   end
 end
