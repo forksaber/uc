ee3fb50bd689f3e1e10067b52ea5f93ef8b16571
Neeraj Bhunwal <neeraj.bhunwal@gmail.com>
2015-12-04 01:44:12 +0530

    simplify ready_wait.rb

--- lib/uc/unicorn/ready_wait.rb
+++ lib/uc/unicorn/ready_wait.rb
@@ -6,20 +6,15 @@ module Uc
   module Unicorn
     class ReadyWait
 
-      include ::Uc::Logger
-      include ::Uc::Unicorn::Helper
+      include Logger
+      include Helper
 
-
-      attr_accessor :run_id
       attr_reader :ready_wait, :server, :worker
 
-
-      def initialize(server, worker, run_id: nil, ready_wait: nil)
+      def initialize(server, worker, ready_wait: nil)
         @server = server
         @worker = worker
-        @run_id = run_id
         @ready_wait = ready_wait
-        @queue_name = "#{event_queue}_ready"
       end
 
       def run
@@ -32,18 +27,14 @@ module Uc
           return
         end
         mq.create
-        msg = mq.wait(event, ready_wait)
+        msg = mq.wait(ready_event, ready_wait)
         event_stream.debug "ack worker ready #{worker.nr}"
       rescue Errno::ENOENT, Errno::EAGAIN, Errno::EACCES, Errno::ETIMEDOUT => e
         event_stream.warn "ready wait error #{worker.nr}: #{e.class}"
       end
 
-      def event
-        run_id ? "ready_#{run_id}" : "ready" 
-      end
-
       def mq
-        @mq ||= ::Uc::Mqueue.new(@queue_name, max_msg: 10, msg_size: 30)
+        @mq ||= ::Uc::Mqueue.new(ready_queue, max_msg: 10, msg_size: 30)
       end
 
     end
