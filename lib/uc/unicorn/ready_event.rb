8dd7da5c6f042f5e84fa76809a71fe5dd5ddc36a
Neeraj Bhunwal <neeraj.bhunwal@gmail.com>
2015-12-04 03:36:27 +0530

    refactor

--- lib/uc/unicorn/ready_event.rb
+++ lib/uc/unicorn/ready_event.rb
@@ -6,19 +6,15 @@ module Uc
   module Unicorn
     class ReadyEvent
 
-      include ::Uc::Logger
-      include ::Uc::Unicorn::Helper
+      include Logger
+      include Helper
 
-      attr_accessor :run_id
       attr_reader :ready_wait, :server, :worker
 
-
-      def initialize(server, worker, run_id: nil, ready_wait: nil)
+      def initialize(server, worker, ready_wait: nil)
         @server = server
         @worker = worker
-        @run_id = run_id
         @ready_wait = ready_wait
-        @queue_name ||= "#{event_queue}_ready"
       end
 
       def run
@@ -26,25 +22,20 @@ module Uc
         if not last_worker?
           notify if ready_wait
         end
-      #    event_stream.pub :fin, "server #{server_event} successful"
       end
 
       def notify
         mq.create
         mq.clear
-        msg = mq.nb_writer do |writer|
-          writer.send event
+        mq.nb_writer do |writer|
+          writer.send ready_event
         end
       rescue Errno::ENOENT, Errno::EAGAIN, Errno::EACCES => e
         event_stream.warn "ready event not sent for worker #{id}: #{e.class}"
       end
 
-      def event
-        run_id ? "ready_#{run_id}" : "ready" 
-      end
-
       def mq
-        @mq ||= ::Uc::Mqueue.new(@queue_name, max_msg: 10, msg_size: 30)
+        @mq ||= ::Uc::Mqueue.new(ready_queue, max_msg: 10, msg_size: 30)
       end
 
     end
